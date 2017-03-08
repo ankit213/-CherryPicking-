c2a688ebf4b7f1c1039ccf695af2c18dc7dce0d0
Ankit Bhanvadia <ankit@promactinfo.com>
2017-03-08 14:01:07 +0530

    load project

---
+++ CherryPicking/CherryPicking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CherryPicking.Startup))]
+namespace CherryPicking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
